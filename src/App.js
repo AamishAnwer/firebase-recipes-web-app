2404757a0f5f5cc5af1d5d496cd0132bd8d289ed
AamishAnwer <anwer.aamish@gmail.com>
2023-05-20 14:37:48 +0530

    maybe prob solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
 import "./App.css";
-
 // eslint-disable-next-line no-unused-vars
 import firebase from "./FirebaseConfig";
 
-function App() {git a
+function App() { 
   return (
     <div className="App">
       <div className="title-row">
